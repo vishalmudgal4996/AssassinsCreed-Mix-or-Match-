af50111632e83907a2ec8208e4259ad2735437d9
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-02 21:07:40 +0530

    audio controller added

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+//For audio inside the game
+
+class AudioController {
+  constructor() {
+    this.bgMusic = new Audio(
+      "Assets/Audio/background_ Assassin's Creed Theme.mp3"
+    );
+    this.flipSound = new Audio("Assets/Audio/flip.wav");
+    this.matchSound = new Audio("Assets/Audio/match_Leap-of-Faith.wav");
+    this.victorySound = new Audio("Assets/Audio/victory.wav");
+    this.gameOverSound = new Audio("Assets/Audio/gameOver.wav");
+    this.bgMusic.volume = 0.5;
+    this.bgMusic.loop = true;
+  }
+  startMusic() {
+    this.bgMusic.play();
+  }
+  stopMusic() {
+    this.bgMusic.pause();
+    this.bgMusic.currentTime = 0;
+  }
+  flip() {
+    this.flipSound.play();
+  }
+  match() {
+    this.matchSound.play();
+  }
+  victory() {
+    this.stopMusic();
+    this.victorySound.play();
+  }
+  gameOver() {
+    this.stopMusic();
+    this.gameOverSound.play();
+  }
+}
+
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", ready());
 } else {
   ready();
 }
 
+//At game startup
 function ready() {
   let overlays = Array.from(document.getElementsByClassName("overlay-text"));
   let cards = Array.from(document.getElementsByClassName("card"));
@@ -11,7 +49,15 @@ function ready() {
   overlays.forEach((overlay) => {
     overlay.addEventListener("click", () => {
       overlay.classList.remove("visible");
+      //game.startGame();
+      let audioController = new AudioController();
+      audioController.startMusic();
     });
   });
 
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      //game.flipCard(Card);
+    });
+  });
 }
